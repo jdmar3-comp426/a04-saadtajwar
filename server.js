8f834288a90645ff5fd3005f2214d17654c8c6c2
Saad Tajwar <saads@ad.unc.edu>
2021-10-28 16:03:51 -0400

    API

--- server.js
+++ server.js
@@ -2,15 +2,17 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
-
+const db = require("./database.js")
 // Require md5 MODULE
+const md5 = require("md5");
+const { response } = require("express");
 
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
-
+const HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
@@ -23,6 +25,14 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.post("/app/new", (req, res) => {
+	const body = req.body;
+	const user = body.user;
+	const pass = body.pass;
+	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)')
+	const info = stmt.run(user, pass);
+	res.json(info);
+})
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
@@ -31,10 +41,21 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {
+	const id = req.params.id;
+	const user = db.prepare(`SELECT * FROM userinfo WHERE id = ${id}`).all();
+	res.status(200).json(user);
+});
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
+app.delete("/app/delete/user/:id", (req, res) => {
+	const id = req.params.id;
+	const stmt = db.prepare('DELETE FROM userinfo WHERE id = ?');
+	const info = stmt.run(id);
+	res.json(info);
+})
 
 // Default response for any other request
 app.use(function(req, res){
